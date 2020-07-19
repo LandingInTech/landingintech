1706bf5a7a52ce3a5dd96dff6566d90c05256f40
FabioRosado <fabioglrosado@gmail.com>
2020-07-19 20:10:09 +0100

    Update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,11 +35,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Landing in Tech`,
+        short_name: `Landing in Tech`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#271B40`,
+        theme_color: `#FC4056`,
         display: `minimal-ui`,
         icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
