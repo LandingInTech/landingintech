6b1eff6c492ca7c66cb058a1e4f4d1ce096dc443
FabioRosado <fabioglrosado@gmail.com>
2020-07-21 11:32:22 +0100

    Add apple podcast url

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import { graphql } from "gatsby"
 import RSS from "../images/icons/rss.svg"
 import SPOTIFY from "../images/icons/spotify.svg"
 import HEADPHONES from "../images/icons/headphones.svg"
+import APPLE from "../images/icons/apple.svg"
 
 const IndexPage = (props) =>  {
 
@@ -18,9 +19,10 @@ const IndexPage = (props) =>  {
       <SEO title="Home | Landing In Tech" />
       <section className="flex justify-center items-center flex-col py-12">
       <LatestEpisode details={episode}/>
-      <div className="flex">
-        <a href="https://feeds.soundcloud.com/users/soundcloud:users:849950239/sounds.rss" className="p-2 green-button flex items-center justify-center mr-2 md:mr-5"><img src={RSS} className="mb-0 mr-2" alt="RSS Feed" /> RSS</a>
-        <a href="https://open.spotify.com/show/6wKJSgr5FSOUMWbuQcVbxh" className="p-2 green-button flex items-center justify-center mr-2 md:mr-5"><img src={SPOTIFY} className="mb-0 mr-2" alt="RSS Feed" /> SPOTIFY</a>
+      <div className="flex flex-col md:flex-row">
+        <a href="https://feeds.soundcloud.com/users/soundcloud:users:849950239/sounds.rss" className="p-2 green-button flex items-center justify-center md:mr-5"><img src={RSS} className="mb-0 mr-2" alt="RSS Feed" /> RSS</a>
+        <a href="https://podcasts.apple.com/gb/podcast/landing-in-tech/id1524211616" className="p-2 green-button flex items-center justify-center md:mr-5"><img src={APPLE} className="mb-0 mr-2" alt="RSS Feed" /> APPLE PODCAST</a>
+        <a href="https://open.spotify.com/show/6wKJSgr5FSOUMWbuQcVbxh" className="p-2 green-button flex items-center justify-center md:mr-5"><img src={SPOTIFY} className="mb-0 mr-2" alt="RSS Feed" /> SPOTIFY</a>
         <a href="https://www.breaker.audio/landing-in-tech" className="p-2 green-button flex items-center justify-center"><img src={HEADPHONES} className="mb-0 mr-2" alt="RSS Feed" /> BREAKER</a>
       </div>
       </section>
