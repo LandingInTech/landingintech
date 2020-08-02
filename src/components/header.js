20ba1b45f859b444991ba7cafe395b812a3a04fa
FabioRosado <fabioglrosado@gmail.com>
2020-08-02 13:15:14 +0100

    update soundcloud links

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ import Logo from "../images/logo.svg"
 const Header = () => (
   <header className="flex justify-center flex-col md:flex-row items-center text-lg">
   <div className="sound-cloud">
-    <iframe className="mb-0" width="100%" height="20" scrolling="no" frameBorder="no" allow="autoplay" title="latest episode" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/860728810&color=%23fc4056&inverse=true&auto_play=false&show_user=true"/>
+    <iframe className="mb-0" width="100%" height="20" scrolling="no" frameBorder="no" allow="autoplay" title="latest episode" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/868848340&color=%23fc4056&inverse=true&auto_play=false&show_user=true"/>
   </div>
   
     <div>
