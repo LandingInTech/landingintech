15a2143d91cb83facbf43915eb0ca5f0f7f820a1
FabioRosado <fabioglrosado@gmail.com>
2020-11-05 22:12:36 +0000

    Create newsletter component and page

---
+++ src/components/newsletter.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react"
+import { useForm } from "react-hook-form"
+
+const Newsletter = () => {
+    const [sent, setSent] = useState(null)
+    const [error, setError] = useState(null)
+    const [disabled, setDisabled] = useState(false)
+
+    const { register, handleSubmit, errors } = useForm()
+
+    const onSubmit = data => {
+        fetch('/.netlify/functions/newsletter', {
+            method: "POST",
+            body: JSON.stringify({
+                first_name: data.name,
+                email: data.email,
+            })
+        })
+        .then(() => {
+            setDisabled(true)
+            setSent(true)
+        })
+        .catch(e => setError(e))
+
+    }
+
+    return (
+          <form className="flex self-center flex-col w-1/2" onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="name">Name</label>
+            <input
+              name="name"
+              placeholder="John Doe"
+              ref={register({ required: true })}
+              autoComplete="name"
+            />
+
+            <label htmlFor="email">Email</label>
+            <input
+              name="email"
+              placeholder="john.doe@example.com"
+              ref={register({
+                required: true,
+                pattern: /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/,
+              })}
+              autoComplete="email"
+            />
+
+            <button
+              className="green-button my-5 p-2 flex justify-center items-center"
+              type="submit"
+              disabled={disabled}
+            >
+              {sent ? (
+                <>
+                  <i className="gg-check mr-2" /> Confirmation email sent
+                </>
+              ) : (
+                <>
+                  <i className="gg-mail mr-2" /> Subscribe to the newsletter!
+                </>
+              )}{" "}
+            </button>
+            <div className="flex flex-col justify-center">
+              {errors.name && (
+                <span className="error">Name field is required.</span>
+              )}
+              {errors.email && (
+                <span className="error">Please enter a valid email.</span>
+              )}
+              {errors.subject && (
+                <span className="error">Subject field is required.</span>
+              )}
+              {errors.message && (
+                <span className="error">Message field is required.</span>
+              )}
+              {error && (<span className="error">Oops! An error occurred when submitting the form,  please try again later.</span>)}
+            </div>
+          </form>
+    )
+}
+
+export default Newsletter
---
+++ src/pages/newsletter.js
@@ -0,0 +1,20 @@
+
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Newsletter from "../components/newsletter"
+
+
+const NewsletterPage = () => (
+    <Layout>
+        <SEO title="Newsletter" />
+        <section className="my-12 flex flex-col mdx">
+            <h1 className="my-12 plane">Subscribe to the Newsletter</h1>
+            <p>On this monthly newsletter, you will get information about episodes releases, upcoming live talks, articles written and pieces of advice from the guests.</p>
+            <Newsletter/>
+        </section>
+    </Layout>
+)
+
+export default NewsletterPage
